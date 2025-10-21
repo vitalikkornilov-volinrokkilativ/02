4fe88a41caf17e98c182cc6a1edf3acd350677ae
vitaly <vitalikkornilov@gmail.com>
2025-10-21 18:50:28 +0300

    commit

---
+++ data.js
@@ -0,0 +1,351 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "схема квартиры",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.016783188387243797,
+        "pitch": 0.9626681661334082,
+        "fov": 1.3774757969026818
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.02331493834185494,
+          "pitch": 0.6582957275671646,
+          "rotation": 3.141592653589793,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.24909004108634036,
+          "pitch": 0.8823214574422664,
+          "rotation": 3.141592653589793,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.3609488923869151,
+          "pitch": 0.7164243691874397,
+          "rotation": 15.707963267948973,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.05880259442467484,
+          "pitch": 1.133176577197494,
+          "rotation": 3.141592653589793,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.5603833419190103,
+          "pitch": 0.7699740206269734,
+          "rotation": 3.141592653589793,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.28707565856347017,
+          "pitch": 0.7354577154342294,
+          "title": "постирочная",
+          "text": "&nbsp;"
+        },
+        {
+          "yaw": 0.3751370928629285,
+          "pitch": 0.6772820613689774,
+          "title": "гардеробная",
+          "text": "&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "гостиная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.18106065755700307,
+        "pitch": 0.11817672962668269,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8315913444272356,
+          "pitch": 0.10580322670380227,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.514402379740728,
+          "pitch": 0.15665068973108554,
+          "rotation": 4.71238898038469,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "столовая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.045264395986510664,
+        "pitch": 0.09780202260261106,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.038288713897039,
+          "pitch": 0.13199010072407624,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.4143249177853363,
+          "pitch": 0.4693935864466887,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.5515690459840723,
+          "pitch": 0.14895485132450403,
+          "rotation": 0.7853981633974483,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.7029124804733922,
+          "pitch": 0.23398477953854346,
+          "rotation": 7.853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2513476835332744,
+          "pitch": 0.1249839628003393,
+          "title": "постирочная",
+          "text": "&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "прихожая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.26005987457779156,
+          "pitch": 0.22981977783577534,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.5482416946318569,
+          "pitch": 0.2286049026493142,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.6881095350270154,
+          "pitch": 0.25786325412491706,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -3.0555350459938335,
+          "pitch": 0.5631263432645284,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.364298253860567,
+          "pitch": 0.22165497087784303,
+          "title": "Постирочная",
+          "text": "&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "спальня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0320481388768084,
+          "pitch": 0.28432795085493723,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6817763663944074,
+          "pitch": 0.1666378911266726,
+          "title": "Гардеробная",
+          "text": "&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.21833414534651396,
+        "pitch": -0.05480659924715425,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23585536369179394,
+          "pitch": 0.11841861986281543,
+          "rotation": 3.141592653589793,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.9272994834386026,
+          "pitch": 0.11844060421345759,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.9619729079683452,
+          "pitch": 0.7234460126731541,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
